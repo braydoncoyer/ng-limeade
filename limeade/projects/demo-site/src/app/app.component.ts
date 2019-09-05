3f95317429453ada993ebfb83921e3c8c48d8c82
Braydon Coyer <braydon.coyer@cognizant.com>
2019-09-05 17:42:10 -0500

    feature/toast-setup: Toasts setup and stacking correctly

--- limeade/projects/demo-site/src/app/app.component.ts
+++ limeade/projects/demo-site/src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import { ToastFactoryService } from 'ng-limeade';
+import { ToastInterface } from '../../../ng-limeade/src/lib/ToastInterface';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'demo-site';
+  constructor(private toastService: ToastFactoryService) {}
+
+  ngOnInit(): void {
+    const toast = {
+      title: 'Warning, Yo!',
+      type: 'warning',
+      description: 'This is a warning from app'
+    };
+    this.toastService.showToast(toast);
+  }
+
+  toast(type: string) {
+    const toast: ToastInterface = {
+      type: type,
+      title: 'A new toast!',
+      description: 'A description!'
+    };
+    this.toastService.showToast(toast);
+  }
 }
--- limeade/projects/ng-limeade/src/public-api.ts
+++ limeade/projects/ng-limeade/src/public-api.ts
@@ -5,3 +5,6 @@
 export * from './lib/ng-limeade.component';
 export * from './lib/ng-limeade.module';
 export * from './lib/toast/toast.component';
+export * from './lib/toast-factory/toast-factory.component';
+export * from './lib/services/toast-factory.service';
+export * from './lib/ToastInterface';
