49c4a3e43c2191786e285566f92cbac76baee1f1
Braydon Coyer <braydon.coyer@cognizant.com>
2019-10-07 09:44:11 -0500

    Toasts fade in and slide in

--- limeade/projects/ng-limeade/src/lib/toast/toast.component.ts
+++ limeade/projects/ng-limeade/src/lib/toast/toast.component.ts
@@ -1,4 +1,5 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, Input, OnInit, Output, EventEmitter} from '@angular/core';
+import { ToastInterface } from '../ToastInterface';
 
 @Component({
   selector: 'limeade-toast',
@@ -10,10 +11,21 @@ export class ToastComponent implements OnInit {
   @Input() type: 'success' | 'info' | 'error' | 'warning';
   @Input() title: string;
   @Input() description: string;
+  @Output() toastClicked: EventEmitter<ToastInterface> = new EventEmitter<ToastInterface>();
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  toastClick() {
+    const toast: ToastInterface = {
+      type: this.type,
+      title: this.title,
+      description: this.description
+    }
+    console.log('Toast Click Method');
+    this.toastClicked.emit(toast);
   }
 
 }
