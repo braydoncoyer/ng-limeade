5baaec5edff47c14308cf1344eb7f88f25a9901d
Braydon Coyer <braydon@coyer.net>
2019-11-05 10:23:12 -0600

    feature/close-button: added font-awesome to library

--- limeade/projects/ng-limeade/src/lib/ng-limeade.module.ts
+++ limeade/projects/ng-limeade/src/lib/ng-limeade.module.ts
@@ -4,16 +4,20 @@ import { ToastComponent } from './toast/toast.component';
 import { CommonModule } from '@angular/common';
 import { ToastFactoryComponent } from './toast-factory/toast-factory.component';
 import { ToastFactoryService } from './services/toast-factory.service';
-
-// export { ToastFactoryService } from './services/toast-factory.service';
+import {FaIconLibrary, FontAwesomeModule} from '@fortawesome/angular-fontawesome';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { far } from '@fortawesome/free-regular-svg-icons';
 
 export {ToastInterface} from './models/ToastInterface';
 
 @NgModule({
   declarations: [NgLimeadeComponent, ToastComponent, ToastFactoryComponent],
-  imports: [ CommonModule ],
+  imports: [CommonModule, FontAwesomeModule],
   providers: [],
   exports: [NgLimeadeComponent, ToastComponent, ToastFactoryComponent]
 })
 export class NgLimeadeModule {
+  constructor(library: FaIconLibrary) {
+    library.addIconPacks(fas, far);
+  }
 }
