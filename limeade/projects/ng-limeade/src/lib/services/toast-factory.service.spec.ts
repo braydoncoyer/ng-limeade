fd8bbf69f9d12ae54ff9aa0e2d251654adb369d1
Braydon Coyer <braydon@coyer.net>
2019-11-15 10:06:14 -0600

    feature/update-tests: Updated and completed tests for toast-factory-service

--- limeade/projects/ng-limeade/src/lib/services/toast-factory.service.spec.ts
+++ limeade/projects/ng-limeade/src/lib/services/toast-factory.service.spec.ts
@@ -1,12 +1,43 @@
 import { TestBed } from '@angular/core/testing';
 
 import { ToastFactoryService } from './toast-factory.service';
+import {Observable} from "rxjs";
+import {ToastInterface} from "../models/ToastInterface";
 
 describe('ToastFactoryService', () => {
+  let service: ToastFactoryService;
   beforeEach(() => TestBed.configureTestingModule({}));
 
+  beforeEach(() => {
+    service = TestBed.get(ToastFactoryService);
+  });
+
   it('should be created', () => {
-    const service: ToastFactoryService = TestBed.get(ToastFactoryService);
     expect(service).toBeTruthy();
+  });
+
+  it('should return subject as observable when getToast is called', () => {
+    const result = service.getToast();
+    expect(result).toEqual(jasmine.any(Observable));
+  });
+
+  it('should push a new toast object through the subject', () => {
+    const mockToast: ToastInterface = {
+      type: 'success',
+      title: 'Woohoo!'
+    };
+    service.getToast().subscribe((res) => {
+      expect(res).toEqual(mockToast);
+    });
+
+    service.showToast(mockToast);
+  });
+
+  it('should push a null value when clear is called', () => {
+    service.getToast().subscribe((res) => {
+      expect(res).toBeNull();
+    });
+
+    service.clear();
   });
 });
